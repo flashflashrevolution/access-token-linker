609bd3f1fa037e97ea116911f79cd344e9a08de8
Zageron <hello@adambryant.ca>
2020-10-31 19:08:12 -0700

    Adjust config to load only if .env exists.

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,19 @@
 import * as dotenv from 'dotenv';
+import fs from 'fs';
+
+const envPath = "./.env";
+
+function loadEnv(err: NodeJS.ErrnoException | null): void
+{
+    if (err == null)
+    {
+        dotenv.config({ path: "./.env" });
+    }
+}
 
 function ImportEnvironmentVariables(): void
 {
-    dotenv.config({ path: "./.env" });
+    fs.stat(envPath, loadEnv);
 }
 
 export { ImportEnvironmentVariables };
