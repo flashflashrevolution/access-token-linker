b5f53c85101920d8027ca61f1a03a120d7a3d1c8
Zageron <hello@adambryant.ca>
2020-10-31 23:44:50 -0700

    Fix error in config loading.

--- src/Config.ts
+++ src/Config.ts
@@ -5,7 +5,12 @@ const envPath = ".env";
 
 async function ImportEnvironmentVariables(): Promise<void>
 {
-    const exists = await fs.stat(envPath);
+    const exists = await fs.stat(envPath)
+        .catch(() =>
+        {
+            console.log(".env does not exist. Load environment variables from secrets or manually.");
+        });
+
     if (exists)
     {
         dotenv.config({ path: envPath });
